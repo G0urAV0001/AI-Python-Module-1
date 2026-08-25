661ee88687304570aec5987338c098bc3e0d0d0c
G0urAV0001 <naikgourav696@gmail.com>
2026-08-25 12:22:37 +0530

    Add variables and data type examples in Python

---
+++ Day-2-Python-Basics/variables.py
@@ -0,0 +1,16 @@
+# Python Variables and Data Types
+
+name = "Gourav"
+age = 20
+percentage = 85.5
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Percentage:", percentage)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(percentage))
+print(type(is_student))
