642a9b3a04298da01ebb01fa200735de3857602c
G0urAV0001 <naikgourav696@gmail.com>
2026-08-25 12:25:19 +0530

    Create functions.py

---
+++ Day-3-Python-Core/functions.py
@@ -0,0 +1,14 @@
+# Python Functions
+
+def add(a, b):
+    return a + b
+
+
+def calculate_average(numbers):
+    return sum(numbers) / len(numbers)
+
+
+print("Addition:", add(10, 20))
+
+marks = [80, 85, 90]
+print("Average:", calculate_average(marks))
