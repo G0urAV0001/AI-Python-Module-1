621bf78001fbd9edc04f982c2d67e6c0bd46b1f0
G0urAV0001 <naikgourav696@gmail.com>
2026-08-25 12:24:15 +0530

    Create lists.py

---
+++ Day-3-Python-Core/lists.py
@@ -0,0 +1,9 @@
+# Working with Lists
+
+marks = [85, 90, 78, 92, 88]
+
+print("Marks:", marks)
+print("Highest mark:", max(marks))
+print("Lowest mark:", min(marks))
+print("Number of marks:", len(marks))
+print("Average:", sum(marks) / len(marks))
